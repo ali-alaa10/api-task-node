71b6a373ca10468150df4218ab97e441d61eedd3
Ali <130253651+ali-alaa10@users.noreply.github.com>
2025-05-05 05:14:22 +0300

    Delete models directory

--- models/Todo.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const todoSchema = new mongoose.Schema({
-  task: { type: String, required: true },
-  done: { type: Boolean, default: false },
-});
-
-export default mongoose.model("Todo", todoSchema);
